5c6e6cb11b8bb4d8e564d2bacd9552599b84ae80
phenyotmoroka-glitch <phenyotmoroka@gmail.com>
2026-03-13 11:43:15 +0200

    Update javas.js

--- js.js
+++ js.js
@@ -1,1 +1,10 @@
-content
+< script >
+function validateForm () {
+let name = document . forms [ " myForm " ][ "
+username " ]. value ;
+if ( name == " " ) {
+alert ( " Name must be filled out " ) ;
+return false ;
+}
+}
+</ script >
